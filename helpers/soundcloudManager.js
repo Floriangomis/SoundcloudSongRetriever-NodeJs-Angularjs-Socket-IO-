8c8a3de028e1755fb3e0a43817fafe24ca7d81fb
Gomis Florian <florian@bunkr.me>
2015-11-27 15:03:33 +0100

    serverside: fix typo

--- helpers/soundcloudManager.js
+++ helpers/soundcloudManager.js
@@ -24,7 +24,7 @@ SoundcloudManager.prototype.initialize = function(){
     this.client = this.sc.client();
     this.client.exchange_token( function( err, token ) {
         if( err ){
-            console.log( 'Erreur exchange_token : ' err );
+            console.log( 'Erreur exchange_token : ' + err );
         }
         this.soundcloudClient = this.sc.client( { access_token : token } );
         this.getFavorites.call( this );
